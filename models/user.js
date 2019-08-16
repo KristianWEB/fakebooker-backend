f78bb96c368468b909055f8413da5e9cf1a23a64
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-08-16 17:24:37 -0400

    added eslint

--- models/user.js
+++ models/user.js
@@ -1,34 +1,39 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 // User Schema
 const UserSchema = mongoose.Schema({
   email: {
+    required: true,
     type: String,
-    required: true
   },
   password: {
+    required: true,
     type: String,
-    required: true
   },
-  roles: { type: [String], default: ["user"] }
+  roles: { default: ['user'] },
+  type: [String],
 });
 
-const User = (module.exports = mongoose.model("User", UserSchema));
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
 
 module.exports.getUserById = (id, callback) => User.findById(id, callback);
 
-module.exports.getUserByEmail = email => User.findOne({ email }).exec();
+module.exports.getUserByEmail = (email) => User.findOne({ email }).exec();
 
-module.exports.addUser = newUser => {
-  bcrypt.genSalt(10, (err, salt) => {
+module.exports.addUser = (newUser) => {
+  bcrypt.genSalt(10, (_, salt) => {
     bcrypt.hash(newUser.password, salt, (err, hash) => {
-      if (err) throw err;
+      if (err) {
+        throw err;
+      }
       newUser.password = hash;
+
       return newUser.save();
     });
   });
 };
 
-module.exports.comparePassword = (candidatePassword, hash) =>
-  bcrypt.compare(candidatePassword, hash);
+module.exports.comparePassword = (password, hash) => bcrypt.compare(password, hash);
