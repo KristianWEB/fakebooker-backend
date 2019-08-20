f1f0c9288223ba992eb5f97d049408fc0686797f
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-08-19 23:30:16 -0400

    removed all files

--- config/database.js
+++
@@ -1,7 +0,0 @@
-const databaseURL = process.env.DB_URL || 'mongodb://localhost:27017/sidekickdb';
-
-module.exports = {
-  database: databaseURL,
-  // Needed for passportjs
-  secret: 'yoursecret',
-};
