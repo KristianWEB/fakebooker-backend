0aed1e45bed631408d89b96b3998985b0566e595
Kristian <46797006+KristianWEB@users.noreply.github.com>
2019-09-16 01:30:52 +0300

    Updated Post Model (#23)
    * Added Comment Model
    
    * Post model updated, fixed model names
    
    * fixed names
    
    * Delete post.js
    
    * Delete user.js

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Comment = require("./Comment");
+
+const PostSchema = mongoose.Schema({
+  source: String, // ObjectId of person who made the post
+  destination: String, // ObjectId of person who received the post(whose wall?)
+  typeOfPost: String, // Type of post[i.e., text, image, url like reddit?]
+  content: String,
+  creationDate: Number,
+  likedBy: [
+    {
+      id: String, // ObjectId of the user,
+    },
+  ],
+  disLikedBy: [
+    {
+      id: String, // ObjectId of the user,
+    },
+  ],
+  edited: Boolean,
+  lastEditedDate: Number,
+  comments: [Comment],
+});
+
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
