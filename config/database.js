fcf2fa62a6ccf5ab0ada91b06a5f77c697450b71
Kacper <kacper.sionkowsk@gmail.com>
2019-09-22 20:29:42 +0200

    Changed database & connected it with server

--- config/database.js
+++ config/database.js
@@ -1,8 +1,30 @@
-const databaseURL =
-  process.env.DB_URL || "mongodb://localhost:27017/sidekickdb";
+const mongoose = require("mongoose");
+// Import config package and get db URI from default.json
+const config = require("config");
+const db = config.get("mongoURI");
 
-module.exports = {
-  database: databaseURL,
-  // Needed for passportjs
-  secret: "yoursecret",
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    // Exit process on failure
+    process.exit(1);
+  }
 };
+
+module.exports = connectDB;
+
+// Previous db connection
+// const databaseURL =
+//   process.env.DB_URL || "mongodb://localhost:27017/sidekickdb";
+
+// module.exports = {
+//   database: databaseURL,
+//   // Needed for passportjs
+//   secret: "yoursecret",
+// };
