f2ed552d132d150437ff233ae0be796a94c96ec1
Moshe Cohen <moshe22cohen@gmail.com>
2019-11-28 20:20:47 +0200

    implementing createComment() - not compelte yet, added typedef to typeDefs.js, added commet.js file to be resolver, resolver added to index.js

--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB_URL: process.env.DB_URL || "mongodb://localhost:27017/sidekickdb",
+  DB_URL: process.env.DB_URL || "mongodb://localhost:27017/sidekick",
   JWT_SECRET: process.env.JWT_SECRET || "YOURSECRET",
   PORT: process.env.PORT || 8080,
 };
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 const testResolvers = require("./hello");
 const usersResolvers = require("./users");
 const postsResolvers = require("./posts");
+const commentsResolvers = require("./comments");//28.11.2019
 
 module.exports = {
   Query: {
@@ -11,5 +12,6 @@ module.exports = {
   Mutation: {
     ...usersResolvers.Mutation,
     ...postsResolvers.Mutation,
+    ...commentsResolvers,//28.11.2019 
   },
 };
