b4deb8ff8026d7e9c4cc4bc2ef7f4fb6af724054
Moshe Cohen <moshe22cohen@gmail.com>
2019-11-28 20:49:55 +0200

    added createComment mutation

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,31 +1,21 @@
-//28.11.2019
-
 const Comment = require("../../models/Comment");
 const getAuthenticatedUser = require("../middlewares/authenticated");
 
 module.exports = {
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      const { user } = getAuthenticatedUser(context);
+      if (!user) {
+        throw new Error("Unauthenticated!");
+      }
 
-    Mutation:{
-        createComment: async(_,{postId,body},context) =>{
-            const { user } = getAuthenticatedUser(context);
-            if (!user) {
-              throw new Error("Unauthenticated!");
-            }
-             
-            if(body.trim() ===''){
+      if (body.trim() === "") {
+      }
 
-            }
-
-            //create comment
-            const comment= Comment({
-
-            });
-              
-
-
-            //return comment
-            
-        }
-    }
-
+    const comment = Comment({
+        body,
+        postId           
+      });
+    },
+  },
 };
