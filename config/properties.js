2961b112a1bc8d4cc67ea329fa52722cc938d2f0
Kristian <devWEBpk@outlook.com>
2020-02-06 19:55:44 +0200

    changed db name

--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB_URL: process.env.DB_URL || "mongodb://localhost:27017/chichi-backend",
+  DB_URL: process.env.DB_URL || "mongodb://localhost:27017/fakebooker",
   JWT_SECRET: process.env.JWT_SECRET || "YOURSECRET",
   PORT: process.env.PORT || 8080,
 };
